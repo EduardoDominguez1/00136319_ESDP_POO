f950ac8c0d2169b916b341ecebb7f36a4f65ae6c
EduardoDominguez1 <00136319@uca.edu.sv>
2020-03-16 20:36:14 -0600

    Add: base de ejercicio 01

---
+++ Labo 01/Ejercicio 01/src/com/ESDP/x00136319/Main.java
@@ -0,0 +1,22 @@
+package com.ESDP.x00136319;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Labo 01");
+        System.out.println("Ejercicio 01");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Ingrese el valor hasta el que se aplicara la serie Fibonacci");
+
+        int n = scan.nextInt();
+        scan.nextInt();
+        int fibo = 1;
+        int fibo2 = 2;
+        int aux = 0;
+
+        }
+    }
+
