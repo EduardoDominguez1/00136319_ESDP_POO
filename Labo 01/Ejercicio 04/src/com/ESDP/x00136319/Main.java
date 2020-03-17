b6ecac60045c6d2d1c73bfa772af8ba854c768b6
EduardoDominguez1 <00136319@uca.edu.sv>
2020-03-17 17:13:25 -0600

    Complete: ejercicio terminado usando .replace

--- Labo 01/Ejercicio 04/src/com/ESDP/x00136319/Main.java
+++ Labo 01/Ejercicio 04/src/com/ESDP/x00136319/Main.java
@@ -1,9 +1,16 @@
 package com.ESDP.x00136319;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
     //Realiza un programa que remplace todos los caracteres "a" por un caracter "b" de una cadena de texto luego muestra la cadena modificada.
-        
+        Scanner sca = new Scanner(System.in);
+        System.out.printf("Ingrese una oracion");
+        String Frase = sca.nextLine();
+        System.out.println("Cambiando a por b");
+        String cambio = Frase.replace("a", "b");
+        System.out.println(cambio);
     }
 }
