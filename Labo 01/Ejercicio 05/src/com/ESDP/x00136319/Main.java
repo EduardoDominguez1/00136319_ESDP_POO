5528c13eda8886c49c79c44c2016444e510cedb2
EduardoDominguez1 <00136319@uca.edu.sv>
2020-03-17 18:22:45 -0600

    Create: inicio de ejercicio05

---
+++ Labo 01/Ejercicio 05/src/com/ESDP/x00136319/Main.java
@@ -0,0 +1,15 @@
+package com.ESDP.x00136319;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Realiza un programa que reciba n letras por teclado y almacene en una lista solo las vocales, mayusculas o minusculas
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese el total de letras que ingresara");
+        int total = scan.nextInt();
+
+        
+    }
+}
