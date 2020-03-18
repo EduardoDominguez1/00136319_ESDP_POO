c3d151c01958d9f03c8e366fdfea4e249c6a2218
EduardoDominguez1 <00136319@uca.edu.sv>
2020-03-17 21:42:20 -0600

    End: usando ventajas de listas y comparando si se encontraban dentro de

--- Labo 01/Ejercicio 05/src/com/ESDP/x00136319/Main.java
+++ Labo 01/Ejercicio 05/src/com/ESDP/x00136319/Main.java
@@ -1,15 +1,37 @@
 package com.ESDP.x00136319;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	//Realiza un programa que reciba n letras por teclado y almacene en una lista solo las vocales, mayusculas o minusculas
+        //Realiza un programa que reciba n letras por teclado y almacene en una lista solo las vocales, mayusculas o minusculas
         Scanner scan = new Scanner(System.in);
         System.out.println("Ingrese el total de letras que ingresara");
-        int total = scan.nextInt();
 
-        
+        List<Character> vocales = Arrays.asList('a', 'e', 'i', 'o', 'u');
+        List<Character> vocalesMayu = Arrays.asList('A', 'E', 'I', 'O', 'U');
+        List<Character> LisVocalesMin = new ArrayList<>();
+        List<Character> LisVocalesMayu = new ArrayList<>();
+        int total = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Ingrese letras 1 por 1");
+        for (int i = 0; i < total; i++) {
+            char letra = scan.nextLine().charAt(0);
+            if (Character.isAlphabetic(letra) && vocales.contains(letra)) {
+                LisVocalesMin.add(letra);
+            }
+            else if(Character.isAlphabetic(letra) && vocalesMayu.contains(letra)) {
+                LisVocalesMayu.add(letra);
+            }
+        }
+        System.out.println("Vocales mayusculas ingresadas");
+        System.out.println(LisVocalesMayu);
+        System.out.println("Vocales minusculas ingresadas");
+        System.out.println(LisVocalesMin);
     }
 }
