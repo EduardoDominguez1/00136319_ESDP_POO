150b1363c1c40b7b28d9bfe92a7107c85565350f
EduardoDominguez1 <00136319@uca.edu.sv>
2020-03-21 03:13:52 -0600

    Add: function 4 and 5 usig for each and 1 getter

--- Labo 02/Ejercicio 01/src/com/ESDP/x00136319/Main.java
+++ Labo 02/Ejercicio 01/src/com/ESDP/x00136319/Main.java
@@ -12,62 +12,82 @@ public class Main {
         String tool, mat;
         int salir = 0;
         double attack, durability, Dps;
-        do{
+        do {
             mostrarMenu();
             System.out.print("Seleccione una opcion: ");
-            byte op = sca.nextByte();sca.nextLine();
-            switch(op){
+            byte op = sca.nextByte();
+            sca.nextLine();
+            switch (op) {
                 case 1:
-                    System.out.print("Herramienta:");
-                    System.out.println("Hacha, Pico, Espada, Pala, Azada"); tool = sca.nextLine();
+                    System.out.println("Herramienta: ");
+                    System.out.println("Hacha, Pico, Espada, Pala, Azada");
+                    tool = sca.nextLine();
 
                     System.out.println("Material:");
-                    System.out.println("Diamante, Oro, Hierro, Piedra, Madera"); mat = sca.nextLine();
+                    System.out.println("Diamante, Oro, Hierro, Piedra, Madera");
+                    mat = sca.nextLine();
 
-                    System.out.print("Ataque: "); attack = sca.nextDouble();sca.nextLine();
-                    System.out.print("Durabilidad: ");durability = sca.nextDouble();sca.nextLine();
-                    System.out.print("Dps: ");Dps = sca.nextDouble(); sca.nextLine();
+                    System.out.print("Ataque: ");
+                    attack = sca.nextDouble();
+                    sca.nextLine();
+                    System.out.print("Durabilidad: ");
+                    durability = sca.nextDouble();
+                    sca.nextLine();
+                    System.out.print("Dps: ");
+                    Dps = sca.nextDouble();
+                    sca.nextLine();
 
                     ListaHerramientas.add(new herramientasMinecraft(tool, mat, attack, durability, Dps));
                     break;
                 case 2:
-                    ConsultAllTools();
+                    System.out.println("Mostrando Herramientas actuales");
                     System.out.println(ListaHerramientas.toString());
                     break;
                 case 3:
-
+                    System.out.println("Consultar un solo tipo de herramientas. . . ");
+                    if (ListaHerramientas.size() == 0) {
+                        System.out.println("No se encuentran Herramientas");
+                        System.out.println("Añada Herramientas\n");
+                    }
+                    else {
+                        System.out.println("Digite el Tipo: ");
+                        System.out.println("Hacha, Pico, Espada, Pala, Azada");
+                        tool = sca.nextLine();
+                        String tipoAgain = tool;
+                        for (int i = 0; i < ListaHerramientas.size(); i++) {
+                            if (ListaHerramientas.get(i).getTipo().equals(tipoAgain)) {
+                                ListaHerramientas.get(i).mostrardatos();
+                            }
+                        }
+                    }
                     break;
-                case 4:
-
-                    break;
-                case 5:
-
-                    break;
-                case 6:
-                salir = 1;
-                    break;
-                default:
-                    System.out.println("Opcion no valida");
-                    break;
+                    case 4:
+                        ListaHerramientas.forEach(s -> {
+                            s.ChopMats();
+                        });
+                        break;
+                        case 5:
+                            ListaHerramientas.forEach(s -> {
+                                s.AttackMob();
+                            });
+                            break;
+                            case 6:
+                                salir = 1;
+                                break;
+                                default:
+                                    System.out.println("Opcion no valida");
+                                break;
             }
-        }while(salir != 1);
+        }while (salir != 1) ;
     }
-    public static void mostrarMenu(){
+    public static void mostrarMenu () {
         System.out.println("--------------------------------------");
         System.out.println("1) Agregar una herramienta");
         System.out.println("2) Consultar todas las herramientas");
-        System.out.println("3) Consultar solo 1 herramienta");
+        System.out.println("3) Consultar solo un tipo de herramientas");
         System.out.println("4) Talar/Picar un bloque");
         System.out.println("5) Atacar un enemigo");
         System.out.println("6) Salir");
         System.out.println("--------------------------------------");
     }
-
-    public static void ConsultAllTools(){
-        System.out.println("Consultando todas las herramientas. . .");
-    }
-    public void ConsultToolOneType(){
-        System.out.println("Consultar una sola herramienta. . . ");
-    }
-
 }
--- Labo 02/Ejercicio 01/src/com/ESDP/x00136319/herramientasMinecraft.java
+++ Labo 02/Ejercicio 01/src/com/ESDP/x00136319/herramientasMinecraft.java
@@ -11,14 +11,24 @@ public class herramientasMinecraft {
         this.durability = durability;
         Dps = dps;
     }
-
+    //Solo se usa un getter
+    public String getTipo() {
+        return tool;
+    }
     //Metodos
     public void ChopMats(){
         System.out.println("Talando. . .");
-
+        System.out.println("Usando " + tool + " De " + mat + "!!!!");
+        System.out.println("PICAS MATERIAL A " + Dps + " Golpes por Segundo!");
+        System.out.println("La durabilidad se ha reducido. .");
+        System.out.println("Durabilidad actual: " + (durability-20));
     }
     public void AttackMob(){
         System.out.println("Atacando. . .");
+        System.out.println("Usando " + tool + " De " + mat + "!!!!");
+        System.out.println("PEGAS A " + Dps + " Golpes por Segundo!");
+        System.out.println("Se ha dañado tu herramienta. .");
+        System.out.println("Durabilidad actual: " + (durability-35));
     }
     //toString
     @Override
@@ -30,5 +40,13 @@ public class herramientasMinecraft {
                 ", durability=" + durability +
                 ", Dps=" + Dps +
                 '}';
+    }
+    public void mostrardatos(){
+        System.out.println("Tipo: "+ tool);
+        System.out.println("Material: "+ mat);
+        System.out.println("Durabilidad: "+ durability);
+        System.out.println("Velocidad de Ataque: "+ Dps);
+        System.out.println("Daño: "+ attack);
+
     }
 }
