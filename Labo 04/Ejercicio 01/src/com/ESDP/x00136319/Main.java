11c491fa363430626c61d22c294a26e019654d02
EduardoDominguez1 <00136319@uca.edu.sv>
2020-04-03 15:02:15 -0600

    Add: Create List and Switch in main and rename folder Inventory

--- Labo 04/Ejercicio 01/src/com/ESDP/x00136319/Main.java
+++
@@ -1,9 +0,0 @@
-package com.ESDP.x00136319;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-
-}
---
+++ Labo 04/Inventory/src/com/ESDP/x00136319/Main.java
@@ -0,0 +1,33 @@
+package com.ESDP.x00136319;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Item> PersonalItems = new ArrayList<>();
+        ArrayList<Item> CommonItems = new ArrayList<>();
+
+        int op = 0;
+        switch (op){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Ingrese una opcion valida");
+        }
+
+    }
+
+}
