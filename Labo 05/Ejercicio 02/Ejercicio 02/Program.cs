256acf03cd2a2e9e09b9e66ec4b591be9102f648
EduardoDominguez1 <00136319@uca.edu.sv>
2020-04-23 15:37:25 -0600

    Add: exercise 02

--- Labo 05/Ejercicio 02/Ejercicio 02/Program.cs
+++ Labo 05/Ejercicio 02/Ejercicio 02/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
+
 
 namespace Ejercicio_02
 {
@@ -8,7 +11,28 @@ namespace Ejercicio_02
         {
             //Hacer una funcion que determine si una frase es palindrome o no e imprima el valor de verdad
             
-            Console.WriteLine("Labo 05");
+            Console.WriteLine("Labo 05 - Ejercicio 02");
+            Console.WriteLine("True = palindrome \nFalse = No es palindrome");
+            Console.WriteLine(palindrome());
+        }
+
+        static bool palindrome(){
+            bool palindrome = false;
+            string palabra, palabraRev;
+            Console.Write("Escriba una palabra: ");
+            
+            palabra = Console.ReadLine();
+            
+            char[] pch = palabra.ToCharArray();
+            
+            Array.Reverse(pch);
+            palabraRev = new string(pch);
+            if(palabra.Equals(palabraRev, StringComparison.OrdinalIgnoreCase))
+            {
+                palindrome = true;
+
+            }
+            return palindrome;
         }
     }
 }
