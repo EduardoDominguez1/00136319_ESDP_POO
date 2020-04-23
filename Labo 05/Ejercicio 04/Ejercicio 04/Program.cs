903d69f26c74f4909bc50ee07c1a7e203369fc6c
EduardoDominguez1 <00136319@uca.edu.sv>
2020-04-23 16:12:23 -0600

    Add: exercise 04

---
+++ Labo 05/Ejercicio 04/Ejercicio 04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realiza un programa que remplace todos los caracteres "a" por un caracter "b" de una cadena de texto luego muestra la cadena modificada.
+            Console.WriteLine("Labo 05 - Ejercicio 04");
+            Console.WriteLine("Programa que remplace todos los caracteres A por un caracter B (no cambia minus si se pone mayus)");
+            Console.Write("Ingrese una frase: ");
+                string frase = Console.ReadLine();
+            Console.WriteLine("A continuacion: ");
+            char a, b;
+            Console.WriteLine("Ingrese el valor de las letras a cambiar");
+            a = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor por el cual cambiaran");
+            b = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Cambiando las " + a + " por "+ b);
+                string fraseCambiada = frase.Replace(a, b);
+            Console.WriteLine(fraseCambiada);
+        }
+    }
+}
