24c3fd5f7a518b8706f9abe64b06e0f4d2d81ed9
EduardoDominguez1 <00136319@uca.edu.sv>
2020-04-23 21:51:36 -0600

    Add: Meet2304 exercise

---
+++ Ejercicios Meet/Meet2304/Meet2304/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.SqlServer.Server;
+
+namespace Meet2304
+{
+    public class Persona
+    {
+        private String nombre, apellido;
+        private int edad;
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+
+        public Persona(String nombre = "Sin nombre",
+            String apellido = "Sin apellido", 
+            int edad = 0)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(nombre)}: {nombre}, {nameof(apellido)}: {apellido}, " +
+                   $"{nameof(edad)}: {edad}";
+        }
+
+        public String saludar(String saludoP)
+        {
+            return nombre + " " + saludoP;
+        }
+    }
+}
