dab12360a69ad4e0596efa19067c97f66111d905
EduardoDominguez1 <00136319@uca.edu.sv>
2020-05-14 15:18:40 -0600

    Add: Labo08

---
+++ Labo 08/Ejercicio 01/Login.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Ejercicio_01
+{
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Labo 08/Ejercicio 01/Register.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ejercicio_01
+{
+    public partial class Register : UserControl
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+    }
+}
